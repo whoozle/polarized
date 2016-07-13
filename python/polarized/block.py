c2aec10026e1fa0227c8ccc7442db31358dd51f1
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-07-13 22:07:19 +0100

    first working prototype, added TERM palette

--- python/polarized/block.py
+++ python/polarized/block.py
@@ -7,47 +7,47 @@ from colormath.color_diff import delta_e_cmc
 import math
 
 class BinaryBlock(object):
-    def __init__(self, data = None, color1 = None, color2 = None):
-        self.color1, self.color2 = color1, color2
-        if data is None or color1 == color2:
-            self.__data = [0 for _ in xrange(0, 64)]
-            return
+	def __init__(self, data = None, color1 = None, color2 = None):
+		self.color1, self.color2 = color1, color2
+		if data is None or color1 == color2:
+			self.__data = [0 for _ in xrange(0, 64)]
+			return
 
-        assert len(data) == 64
-        self.__data = []
-        lab1 = convert_color(sRGBColor(*color1, is_upscaled = True), LabColor)
-        lab2 = convert_color(sRGBColor(*color2, is_upscaled = True), LabColor)
-        for rgb in data:
-            rgb = sRGBColor(*rgb, is_upscaled = True)
-            lab = convert_color(rgb, LabColor)
-            d1 = delta_e_cmc(lab, lab1, 1, 1)
-            d2 = delta_e_cmc(lab, lab2, 1, 1)
-            self.__data.append(d1 / (d1 + d2))
-        assert len(self.__data) == 64
+		assert len(data) == 64
+		self.__data = []
+		lab1 = convert_color(sRGBColor(*color1[1], is_upscaled = True), LabColor)
+		lab2 = convert_color(sRGBColor(*color2[1], is_upscaled = True), LabColor)
+		for rgb in data:
+			rgb = sRGBColor(*rgb, is_upscaled = True)
+			lab = convert_color(rgb, LabColor)
+			d1 = delta_e_cmc(lab, lab1, 1, 1)
+			d2 = delta_e_cmc(lab, lab2, 1, 1)
+			self.__data.append(d1 / (d1 + d2))
+		assert len(self.__data) == 64
 
-    def rms(self, data):
-        rms = 0
-        for c1, c2 in zip(self.__data, data):
-            d = (c1 - c2)
-            rms += d * d
-        return math.sqrt(rms / len(data))
+	def rms(self, data):
+		rms = 0
+		for c1, c2 in zip(self.__data, data):
+			d = (c1 - c2)
+			rms += d * d
+		return math.sqrt(rms / len(data))
 
 class Block(object):
-    def __init__(self, data = None):
-        if data is None:
-            data = [(0, 0, 0) for _ in xrange(0, 64)]
-        assert len(data) == 64
-        self.__data = data
+	def __init__(self, data = None):
+		if data is None:
+			data = [(0, 0, 0) for _ in xrange(0, 64)]
+		assert len(data) == 64
+		self.__data = data
 
-    def set(self, y, x, rgb):
-        self.__data[y * 8 + x] = rgb
+	def set(self, y, x, rgb):
+		self.__data[y * 8 + x] = rgb
 
-    def quantize(self, maxColors):
-        p = Palette(maxColors)
-        for rgb in self.__data:
-            p.addColor(rgb)
-        return p.quantize()
+	def quantize(self, maxColors):
+		p = Palette(maxColors)
+		for rgb in self.__data:
+			p.addColor(rgb)
+		return p.quantize()
 
-    def split(self, picker):
-        color1, color2 = map(lambda c: picker.getColor(c), self.quantize(2))
-        return BinaryBlock(self.__data, color1, color2)
+	def split(self, picker):
+		color1, color2 = map(lambda c: picker.getColor(c), self.quantize(2))
+		return BinaryBlock(self.__data, color1, color2)
--- python/polarized/image.py
+++ python/polarized/image.py
@@ -1,20 +1,20 @@
 from block import Block
 
 class Image(object):
-    def __init__(self, w, h):
-        self.width, self.height = w, h
-        self.bw, self.bh = (w + 7) / 8, (h + 7) / 8
-        self.__blocks = []
-        for i in xrange(0, self.bw * self.bh):
-            self.__blocks.append(Block())
+	def __init__(self, w, h):
+		self.width, self.height = w, h
+		self.bw, self.bh = (w + 7) / 8, (h + 7) / 8
+		self.__blocks = []
+		for i in xrange(0, self.bw * self.bh):
+			self.__blocks.append(Block())
 
-    def set(self, y, x, rgb):
-        by, bx = y / 8, x / 8
-        x %= 8
-        y %= 8
-        block = self.__blocks[by * self.bw + bx]
-        block.set(y, x, rgb)
+	def set(self, y, x, rgb):
+		by, bx = y / 8, x / 8
+		x %= 8
+		y %= 8
+		block = self.__blocks[by * self.bw + bx]
+		block.set(y, x, rgb)
 
-    @property
-    def blocks(self):
-        return self.__blocks
+	@property
+	def blocks(self):
+		return self.__blocks
